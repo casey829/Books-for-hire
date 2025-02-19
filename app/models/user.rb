35e14bcef12f8c7dd4fcc8524bb54e1d60f0cdfe
casey829 <caseymwangi31@gmail.com>
2025-02-19 18:53:44 +0300

    updating the user model on the application record

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :borrowings, dependent: :destroy
+  has_many :books, through: :borrowings
+  has_many :active_borrowings, -> { where(returned_at: nil)},
+           class_name: 'Borrowing'
+
+  has_many :currently_borrowed_books,
+           through: :active_borrowings,
+           source: :book
+
+  def can_borrow_more_books?
+    active_borrowings.count < 5
+end
+
+  def has_overdue_books?
+    active_borrowings.where('due_date < ?', Date.current).exists?
+  end
 end
